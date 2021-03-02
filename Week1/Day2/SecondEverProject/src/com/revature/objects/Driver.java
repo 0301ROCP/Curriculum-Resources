fbd556e72a311949a7bb811d524a4e5445b9e136
BenArayathel <ben.arayathel@gmail.com>
2021-03-02 12:50:45 -0500

    object attributes and methods

--- Week1/Day2/SecondEverProject/src/com/revature/objects/Driver.java
+++ Week1/Day2/SecondEverProject/src/com/revature/objects/Driver.java
@@ -18,16 +18,40 @@ public class Driver {
 		 * 
 		 */
 		
-//		Fruit myFruit = new Fruit();
-//		
-//		myFruit.fruitType = "Apples";
-//		myFruit.price = 1;
-//		
-//		myFruit = new Fruit();
+		Fruit myFruit = new Fruit();
+		Fruit myFruit2 = new Fruit();
+		
+//		myFruit.toRipe(); //this right here is invoking a method in the object 
+		
+		myFruit.fruitType = "Apples";
+		myFruit.price = 1;
+		
+//		myFruit.toRot();
+//		myFruit.toRot();
+//		myFruit.toRot();
+//		myFruit.toRot();
+//		myFruit.toRot();
+//		myFruit.toRot();
+//		myFruit.toRot();
+//		myFruit.discountPrice(0); //Now that the fruit has rotted away, the price is equal to 0;
 //		
 //		System.out.println(myFruit.fruitType);
 //		System.out.println(myFruit.price);
 //		
+//		myFruit2.discountPrice(20);
+//		myFruit2.toRot();
+		
+//		double myBigDouble = myFruit2.addNumbers(10, 0); //implicitly cast from int to double 
+//		
+//		System.out.println( myBigDouble);
+//		
+//		double dummy1  = 0.1;
+//		double dummy2 = 7.5;
+//		
+//		myFruit.addNumbers(dummy1, dummy2);
+//		myFruit.onlyAddInts((int) dummy1, (int) dummy2); //in this case we're trying to cast from double -> int
+														// we're losing information, so we have to expliclity tell it to cast. 
+		
 //		System.out.println(myFruit.toString());
 		
 		
@@ -39,27 +63,35 @@ public class Driver {
 		
 		myHuman3 = myHuman; // We lose any ability to reference Human3, Stevey. 
 		
-		myHuman.age = 102;
-		myHuman.height = 200;
-		myHuman.weight = 200;
-		myHuman.name = "Bobby";
+//		myHuman.age = 102;
+//		myHuman.height = 200;
+//		myHuman.weight = 200;
+//		myHuman.name = "Bobby";
+		
+		myHuman.toMakeABobby(102, 200, 200);
 		myHuman.myShoes = new Shoe[10]; // I lose the reference to the original array 
 										// the original array had references to the 3 shoes 
 										// by extension, I lose reference to the shoes as well. 
-//		
-//		
-//		
-		myHuman3.name = "Franky";
+		
+
+		
+//		myHuman.givingDetails();
+//		myHuman2.givingDetails();
+		
+		myHuman.pimpMyShoes();
+		myHuman.showOfMyShoes();
+		
+//		myHuman3.name = "Franky";
 		
 //		System.out.println(myHuman.name); //Franky  
 //		System.out.println(myHuman2.name); //null    
 //		System.out.println(myHuman3.name); //Franky  
 		
-		System.out.println(myHuman.organs);
-		
-		for(String organ: myHuman.organs) {
-			System.out.println(organ);
-		}
+//		System.out.println(myHuman.organs);
+//		
+//		for(String organ: myHuman.organs) {
+//			System.out.println(organ);
+//		}
 		
 	}
 
